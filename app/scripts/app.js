18f6240f7bfa8ae6607dbb672f8911290fae528d
ghar <pete@elusivestars.com>
2015-02-26 01:21:27 -0500

    base yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name learningAngularJsApp
+ * @description
+ * # learningAngularJsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('learningAngularJsApp', []);
