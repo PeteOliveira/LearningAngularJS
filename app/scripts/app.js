eb51920fc952bb0afc2564bc4ff91abce174294b
ghar <pete@elusivestars.com>
2015-02-28 17:27:25 -0500

    Added more gems to app.js and used ng-repeat to show

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,15 +3,38 @@
   var app = angular.module('store', []);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
+  var gems = [
+    {
     name: 'Dodecahedron',
     price: 2.95,
     description: 'very cool gem',
-    canPurchase: false,
-    soldOut: true
+        canPurchase: false,
+      soldOut: true
+    },
+     {
+      name: 'Pentagonal',
+      price: 1.95,
+      description: 'very bright gem',
+      canPurchase: false,
+      soldOut: false
+    },
+    {
+      name: 'Hexagonal',
+      price: 3.95,
+      description: 'very nice gem',
+      canPurchase: true,
+      soldOut: true
+   },
+   {
+     name: 'Octagonal',
+     price: 4.95,
+     description: 'exceptional gem',
+     canPurchase: true,
+     soldOut: false
   }
+];
 
 })();
